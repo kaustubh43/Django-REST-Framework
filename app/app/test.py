27ca39e930916f1a000f60761b0b6f6f3a07b81f
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2023-11-30 21:27:34 +0530

    day5-linting-corrections

--- app/app/test.py
+++ app/app/test.py
@@ -2,11 +2,11 @@
 Sample test
 """
 from django.test import SimpleTestCase
-
 from app import calc 
 
 class CalcTest(SimpleTestCase):
     """Test the calc method"""
+
 
     def test_add_number(self):
         """Test adding numbers together"""
@@ -14,9 +14,8 @@ class CalcTest(SimpleTestCase):
 
         self.assertEqual(res, 11)
 
-
     def test_subtract_numbers(self):
         """"Subtracting numbers"""
-        res = calc.subtract(10,15)
+        res = calc.subtract(10, 15)
 
         self.assertEqual(res, -5)
